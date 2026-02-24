621b00aab26b9282d2365be0865737d75114592f
JAOV <126438253+jaov2000@users.noreply.github.com>
2026-02-24 16:30:52 -0500

    Reto #1

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jaov2000.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jaov2000.py
@@ -17,4 +17,31 @@ en diferentes lineas
 my_var = 'Mi Variable'
 my_var = 'Actualizacion de mi variable'
 
-MY_CONSTANT = "Mi constante" #Por convencion 
+MY_CONSTANT = "Mi constante" #Por convencion, se indica que es una constante pero en realidad puede variar
+
+My_integer = 5
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_str = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+message = "¡Hola, Python!"
+print(message)
+
+print(type(My_integer))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_str))
+
+#Hay formas mas directas para indicar el tipo de una variable, solo funciona para dar
+#No limita el cambio del tipo de variable mas adelante
+
+
+my_int: int = 5
+
+
+
+
+
+
