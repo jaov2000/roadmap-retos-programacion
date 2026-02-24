50c9e9450e144ca35ddb9a9be5509f4282ff0c67
JAOV <126438253+jaov2000@users.noreply.github.com>
2026-02-24 16:11:00 -0500

    Reto #1

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jaov2000.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jaov2000.py
@@ -1,1 +1,20 @@
+#https://www.python.org/
 
+#Comentario de una linea 
+
+"""
+Comentario
+en multiples 
+lineas
+"""
+
+'''
+Esto tambien es
+un comentario
+en diferentes lineas
+'''
+
+my_var = 'Mi Variable'
+my_var = 'Actualizacion de mi variable'
+
+MY_CONSTANT = "Mi constante" #Por convension 
